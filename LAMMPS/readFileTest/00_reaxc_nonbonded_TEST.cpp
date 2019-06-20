83ae94dc1e6576dc4c67ca5c5cf8c421a6959945
mattc2x4 <38536027+mattc2x4@users.noreply.github.com>
2019-06-20 14:24:28 -0400

    LAMMPS

--- LAMMPS/readFileTest/00_reaxc_nonbonded_TEST.cpp
+++ LAMMPS/readFileTest/00_reaxc_nonbonded_TEST.cpp
@@ -51,7 +51,7 @@ void vdW_Coulomb_Energy( reax_system *system, control_params *control,
 	int nRowsrest;
 	double** restMatrix; 	// pointer to pointer (dynamic memory allocation); is deallocated at end of file
 	std::ofstream testFile;		//this is where we initialize our test file, which we will be writing to
-	testFile.open("testWrite.txt", std::ios_base::app);
+	testFile.open("nonbonded_TEST.txt", std::ios_base::app);
 
 
 	// read in the restraint parameter data file
@@ -84,36 +84,40 @@ void vdW_Coulomb_Energy( reax_system *system, control_params *control,
 	F2rest = restMatrix[restk][4];
 	Rminrest = restMatrix[restk][5];
 	Rmaxrest = restMatrix[restk][6];
+	testFile <<"\n\nCACLULATING RESTRAINT FORCE";
 	testFile << "id1rest: "<<id1rest<< " id2rest: "<<id2rest<< " R12rest: "<<R12rest<< " F1rest: "<<F1rest<< " F2rest: "<<F2rest<< " Rminrest: "<<Rminrest<< " Rmaxrest: "<<Rmaxrest<<"\n";
-	testFile<<"id1 compared to: "<< system->my_atoms[i].orig_id<<"   id2 compared to "<<system->my_atoms[i].orig_id;
     for ( i = 0; i < (natoms-1); i++ ) {
-		if ( (id1rest == system->my_atoms[i].orig_id) or (id2rest == system->my_atoms[i].orig_id) ) {				//this line is puzzling, have to figure out what happens here.
+    	testFile<<"id1 compared to: "<< system->my_atoms[i].orig_id<<" = "<<(id1rest == system->my_atoms[i].orig_id)<<"   id2 compared to "<<system->my_atoms[i].orig_id<<" = "<<(id2rest == system->my_atoms[i].orig_id)<<"\n";
+		if ( (id1rest == system->my_atoms[i].orig_id) or (id2rest == system->my_atoms[i].orig_id) ) {
 			for ( j = (i+1); j < natoms; j++) {
+				testFile<<"id1 compared to: "<< system->my_atoms[j].orig_id<<" = "<<(id1rest == system->my_atoms[j].orig_id)<<"   id2 compared to "<<system->my_atoms[j].orig_id<<" = "<<(id2rest == system->my_atoms[j].orig_id)>>"\n";
 				if ( (id1rest == system->my_atoms[j].orig_id) or (id2rest == system->my_atoms[j].orig_id) ) {
                     dxrest = system->my_atoms[i].x[0] - system->my_atoms[j].x[0];
                     dyrest = system->my_atoms[i].x[1] - system->my_atoms[j].x[1];
                     dzrest = system->my_atoms[i].x[2] - system->my_atoms[j].x[2];
                     Rijrest = sqrt(dxrest*dxrest + dyrest*dyrest + dzrest*dzrest);
-                    testFile<< "dxrest: "<<system->my_atoms[i].x[0]<<" - "<<system->my_atoms[j].x[0]<<"\n";
-                    testFile<< "dyrest: "<<system->my_atoms[i].x[1]<<" - "<<system->my_atoms[j].x[1]<<"\n";
-                    testFile<< "dzrest: "<<system->my_atoms[i].x[2]<<" - "<<system->my_atoms[j].x[2]<<"\n";
+                    testFile<< "dxrest: "<<system->my_atoms[i].x[0]<<" - "<<system->my_atoms[j].x[0]<<" = "<<dxrest<<"\n";
+                    testFile<< "dyrest: "<<system->my_atoms[i].x[1]<<" - "<<system->my_atoms[j].x[1]<<" = "<<dyrest<<"\n";
+                    testFile<< "dzrest: "<<system->my_atoms[i].x[2]<<" - "<<system->my_atoms[j].x[2]<<" = "<<dzrest<<"\n";
                     testFile<< "Rijrest: "<<Rijrest<<"\n";
-					if ((Rijrest < Rmaxrest) and (Rijrest > Rminrest)) {
+    				testFile<<"is Rijrest < Rmaxrest, and Rijrest > Rminrest "<<((Rijrest < Rmaxrest) and (Rijrest > Rminrest));
+                    if ((Rijrest < Rmaxrest) and (Rijrest > Rminrest)) {
 						Erest = F1rest*(1.0 - exp(-1.0*F2rest*(Rijrest - R12rest)*(Rijrest - R12rest) ));
 						dErestdr = 2.0*F1rest*F2rest*(Rijrest - R12rest)*exp(-1.0*F2rest*(Rijrest - R12rest)*(Rijrest - R12rest));
 						dErestx = dErestdr*dxrest/Rijrest;
 						dEresty = dErestdr*dyrest/Rijrest;
 						dErestz = dErestdr*dzrest/Rijrest;
-						testFile<< "Erest :"<<Erest<<"\n";
-						testFile<< "dErestdr :"<<dErestdr<<"\n";
-						testFile<< "dErestx :"<<dErestx<<"\n";
-						testFile<< "dEresty :"<<dEresty<<"\n";
-						testFile<< "dErestz :"<<dErestz<<"\n";
+						testFile<< "Erest: "<<Erest<<"\n";
+						testFile<< "dErestdr: "<<dErestdr<<"\n";
+						testFile<< "dErestx: "<<dErestx<<"\n";
+						testFile<< "dEresty: "<<dEresty<<"\n";
+						testFile<< "dErestz: "<<dErestz<<"\n";
 						temp[0] = dErestx;
 	                    temp[1] = dEresty;
 	                    temp[2] = dErestz;
 	                    rvec_Add( workspace->f[i], temp);
 	                    rvec_ScaledAdd( workspace->f[j], -1.0 ,temp);
+	                    testFile<<"dErestx, dEresty, dErestz added to workspace->f["<< i<<"]";
 					}
 				}
 			}
