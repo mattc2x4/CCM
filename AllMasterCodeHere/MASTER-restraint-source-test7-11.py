df868844bab5fa3ec9aaf5693a91589b38750daa
mattc2x4 <38536027+mattc2x4@users.noreply.github.com>
2019-07-18 11:58:14 -0400

    work

--- AllMasterCodeHere/MASTER-restraint-source-test7-11.py
+++ AllMasterCodeHere/MASTER-restraint-source-test7-11.py
@@ -63,15 +63,6 @@ F2CN = 0.75
 R12OC = 1.95
 R12OH = 1.05
 R12CN = 1.4
-#distances [min,max] within which we consider the bond to be made. +/- 15% of above equilibrium distances
-CObondDist = [2.3 , 2.9]
-OHbondDist = [.85 , 1.2]
-CNbondDist = [1.25 , 1.61]
-NHbondDist = [2.1 , 3.2]
-
-#where we put the formed bonds found in findSuccess
-bondID = []
-
 
 #time step
 timestep = 10000
@@ -85,7 +76,6 @@ timestep = 10000
 #TO-DO: find a way to include the exact time step in each instance of restraint.
 newfile = open("rest-ALLdata.txt",'a')
 coordFile = open("coord.txt",'a')
-bondFile = open("pyBond.txt",'a')
 
 
 
@@ -129,13 +119,10 @@ def main():
             #coordFile.write("Atom Index: "+ str(o) + " X: "+ str(coordinates[3*o]) + " Y: "+ str(coordinates[3*o+1]) + " Z: " + str(coordinates[3*o+2]))
             #coordFile.write("\n")
         search(natoms, atomType, coordinates,currentStep)
-        findSuccessBonds(natoms, atomType, coordinates,currentStep)
         lmp1.command("run " + str(timestep)) # lmp1.command("run 100000")
  
     newfile.close()
     coordFile.close()
-    bondFile.close()
-
     # add additional commands to lammps instance and run additional steps
 
 # =============================================================================
@@ -167,7 +154,6 @@ def search(natoms, atomType, c,currentStep): # c = coordinates
     # data file "rest-data.txt"
     # in nested for loops: i = Carbon, j = Oxygen, k = Nitrogen, m = Hydrogen; don't confuse address with id; address = id - 1
     restID = []
-    coordFile.write("in Search\n")
     for i in range(0,natoms):
         if atomType[i] == Ctype:
             for j in range(0,natoms):
@@ -185,6 +171,35 @@ def search(natoms, atomType, c,currentStep): # c = coordinates
                                     newfile.write("\nO\n ID: " + str(j+1) + " X: " + str(c[j*3]) + " Y: " + str(c[j*3+1]) + " Z: " + str(c[j*3+2]))
                                     newfile.write("\nN\n ID: " + str(k+1) + " X: " + str(c[k*3]) + " Y: " + str(c[k*3+1]) + " Z: " + str(c[k*3+2]))
                                     newfile.write("\nH\n ID: " + str(m+1) + " X: " + str(c[m*3]) + " Y: " + str(c[m*3+1]) + " Z: " + str(c[m*3+2]))
+                                    
+    # end of nested loops
+    # this block checks to remove competing groups, and allows closest groups to pass. if C or N is same ID between 2 groups, they are competing
+    #no need to worry about index out of bounds when deleting, seems for loops are modified when you use the del command.
+#    delArray = []
+#    for i in range(0,len(restID)):
+#        for j in range(0,len(restID)):
+#            if (restID[i][0] == restID[j][0] and (i != j)):       #if groups share C
+#                if (getPerim(restID[j],c) <= getPerim(restID[i],c)):     #if the Perimeter of the group at j is less than Perimeter distance of the group at i, delete group at i. else delete group at j
+#                    coordFile.write("deleting group: " + str(restID[i]) + "\n")
+#                    coordFile.write(str(restID[i]) + "Perim = " + str(getPerim(restID[i],c)) + "\n")
+#                    coordFile.write(str(restID[j]) + "Perim = " + str(getPerim(restID[j],c)) + "\n")
+#                    delArray.append(i)       #adds i to delArray, which will be deleted after completion of loops.
+#                else:
+#                    coordFile.write("deleting group: " + str(restID[j]) + "\n")
+#                    coordFile.write(str(restID[i]) + "Perim = " + str(getPerim(restID[i],c)) + "\n")
+#                    coordFile.write(str(restID[j]) + "Perim = " + str(getPerim(restID[j],c)) + "\n")
+#                    delArray.append(j)
+#            if (restID[i][2] == restID[j][2] and (i != j)):       #if groups share N
+#                 if (getPerim(restID[j],c) <= getPerim(restID[i],c)):     #if the Perimeter of the group at j is less than Perimeter distance of the group at i, delete group at i. else delete group at j
+#                     coordFile.write("deleting group: " + str(restID[i]) + "\n")
+#                     coordFile.write(str(restID[i]) + "Perim = " + str(getPerim(restID[i],c)) + "\n")
+#                     coordFile.write(str(restID[j]) + "Perim = " + str(getPerim(restID[j],c)) + "\n")
+#                     delArray.append(i)       #deletes array at i
+#                 else:
+#                     coordFile.write("deleting group: " + str(restID[j]) + "\n")
+#                     coordFile.write(str(restID[i]) + "Perim = " + str(getPerim(restID[i],c)) + "\n")
+#                     coordFile.write(str(restID[j]) + "Perim = " + str(getPerim(restID[j],c)) + "\n")
+#                     delArray.append(j)
     for i in range(0,len(restID)):      #you win python, this is such a stupid way to do this, and I hate you.  Calls findOptimal for the len, and if it doesn't return -1 it deletes at the index
         delInd = findOptimal(restID, c)
         if (delInd != -1):
@@ -198,7 +213,7 @@ def search(natoms, atomType, c,currentStep): # c = coordinates
 
     restfile = open("rest-data.txt",'w')
     restfile.write(str(len(restID) * 3))        #number of groups we would apply force to TODO: must check for distances.
-    #newfile.write("\n" + "timestep: " + str(currentStep))
+    newfile.write("\n" + "timestep: " + str(currentStep))
     newfile.write(str(len(restID) * 3))
     for i in range(0,len(restID)):
         #O,C
@@ -214,11 +229,7 @@ def search(natoms, atomType, c,currentStep): # c = coordinates
         
     restfile.close()
     
-  #  newfile.write("\n" + "timestep: " + str(currentStep))
-#    if (str(len(restID)) == 0):
-#        newfile.close()
-#        coordFile.close()
-
+    newfile.write("\n" + "timestep: " + str(currentStep))
         
     return 0
 
@@ -241,8 +252,9 @@ def initialize():
 def getPerim(atomGroup,coord):
     return distance(atomGroup[0],atomGroup[1],coord) + distance(atomGroup[0],atomGroup[3],coord) + distance(atomGroup[1],atomGroup[2],coord) + distance(atomGroup[2],atomGroup[3],coord)
 
-#python really got me here.  Basically call this for as many items in the list. I really should think of a better way, but python. returns an index that is inoptimal (to be deleted) or -1 ( if none are inoptimal)
+#python really got me here.  Basically call this for as many items in the list. I really should think of a better way, but python doodoo. returns an index that is inoptimal (to be deleted) or -1 ( if none are inoptimal)
 def findOptimal(restID, c):
+    coordFile.write("in search")
     for i in range(0,len(restID)):
         for j in range(0,len(restID)):
             if (restID[i][0] == restID[j][0] and (i != j)):       #if groups share C
@@ -268,31 +280,4 @@ def findOptimal(restID, c):
                      coordFile.write(str(restID[j]) + "Perim = " + str(getPerim(restID[j],c)) + "\n")
                      return j
     return -1
-
-#This function will search for successfully created bonds
-def findSuccessBonds(natoms, atomType, c,currentStep):
-    bondFile = open("pyBond.txt",'a')
-    for i in range(0,natoms):
-        if atomType[i] == Ctype:
-            for j in range(0,natoms):
-                if ((atomType[j] == Otype) and (CObondDist[0] < distance(i,j,c)) and (CObondDist[1] > distance(i,j,c))):
-                    for k in range(0,natoms):
-                        if ( (atomType[k] == Ntype) and (CNbondDist[0] < distance(i,k,c)) and (CNbondDist[1] > distance(i,k,c))):
-                            for m in range(0,natoms):
-                                if ( (atomType[m]==Htype) and (OHbondDist[0] < distance(j,m,c)) and (OHbondDist[1] > distance(j,m,c))):
-                                    # +1 means address converted to id
-                                    #[[C,O,N,H],...]
-                                    # and (NHbondDist[0] < distance(k,m,c)) and (NHbondDist[1] > distance(k,m,c)) 
-                                    if ([i+1,j+1,k+1,m+1] not in bondID ):
-                                        bondID.append([i+1,j+1,k+1,m+1])
-                                        bondFile.write("\ntimestep:" + str(currentStep) + "\n")
-                                        bondFile.write("bondID array: " + str(bondID) + "\n")
-                                        bondFile.write("\nATOMS FOUND: timestep: " + str(currentStep) + " \nC\n ID: " + str(i+1) + " X: " + str(c[i*3]) + " Y: " + str(c[i*3+1]) + " Z: " + str(c[i*3+2]))
-                                        bondFile.write("\nO\n ID: " + str(j+1) + " X: " + str(c[j*3]) + " Y: " + str(c[j*3+1]) + " Z: " + str(c[j*3+2]))
-                                        bondFile.write("\nN\n ID: " + str(k+1) + " X: " + str(c[k*3]) + " Y: " + str(c[k*3+1]) + " Z: " + str(c[k*3+2]))
-                                        bondFile.write("\nH\n ID: " + str(m+1) + " X: " + str(c[m*3]) + " Y: " + str(c[m*3+1]) + " Z: " + str(c[m*3+2]))
-                                    
-
-                            
-                                    
 main()
---
+++ LAMMPS/readFileTest/copied_nonbonded.cpp
@@ -0,0 +1,10 @@
+/*
+ * copied_nonbonded.cpp
+ *
+ *  Created on: Jul 1, 2019
+ *      Author: mattcohe
+ */
+
+
+
+
