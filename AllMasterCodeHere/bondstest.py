f9e60f307cc5edc6007473c549b3b7e26ee2be81
mattc2x4 <38536027+mattc2x4@users.noreply.github.com>
2019-08-13 16:05:30 -0400

    work

--- AllMasterCodeHere/bondstest.py
+++ AllMasterCodeHere/bondstest.py
@@ -16,6 +16,8 @@ Hlist = []
 Nlist = []
 bondList = []
 bondnum = 0
+start = False
+
 stored = False
 for i in range(natoms):     #gets all carbon locations.  won't be ID till end. 
     if (atomType[i] == Ctype):
@@ -30,14 +32,16 @@ totalBond = 0
 f = open("testbonds.txt", "r")
 lineFile = f.readlines()
 formedBonds = []
-currStep = 0
+currStep = -1
 for line in lineFile:       #loops through each line of the file
     stored = False
     wordList = line.split()     #splits the line into a list of words dilineated by any space
-    if (wordList[0] == '#' and wordList[1] == 'Timestep'):      # the hashtag starts the header area
-        currStep = wordList[2]      #grabs and stores timestep
-    elif (wordList[0] != '#'):      #into the body of the file
-        findPairs()
+    if (wordList[0] == '#' and wordList[1] == 'Timestep' and int(wordList[2]) == currentStep):      # the hashtag starts the header area
+        int(currStep) = wordList[2]      #grabs and stores timestep
+        start = True
+    if (wordList[0] != '#' and start):      #into the body of the file
+        if (currStep == 0):
+            findPairs(lineFile,Clist,Olist,Nlist,Hlist)
         if (wordList[0] in Clist):      #THis section checks for N-C bonds, if first is C
             for group in bondList:
                 if (wordList[0] in bondList[group]):        #and we haven't already stored this C
@@ -47,24 +51,25 @@ for line in lineFile:       #loops through each line of the file
                 bondnum = wordList[3]       #gets number of bond this atom has.
                 for i in range(bondnum):
                     if (wordList[3 + i] in Nlist):
-                        bondList.append([wordList[0],-1,wordList[3+i],-1])        #adds C and N to an array as a group.  -1 are placeholders for O and H, and should be in the form [C,O,N,H] to follow previous group notation.
-        elif (wordList[0] in Nlist):
+                        bondList.append([wordList[0],-1,wordList[3+i],-1,currStep])        #adds C and N to an array as a group.  -1 are placeholders for O and H, and should be in the form [C,O,N,H,step] to follow previous group notation.
+        elif (wordList[0] in Olist):
             for group in bondList:
-                if (wordList[0] in bondList[group]):        #and we haven't already stored this C
+                if (wordList[0] in bondList[group]):        #and we haven't already stored this N
                     stored = True
                     break
             if (not stored):
                 bondnum = wordList[3]
                 for i in range(bondnum):
-                    
+                    if (wordList[3 + i] in Hlist):
+                        bondList.append(-1,[wordList[0],-1,wordList[3+i], currStep])
             
-            
+start = False           
                         
 def getCO (Clist,Olist,wordList):    
     #this should pull CO pairs from timestep 0.
     #to be referenced and called in getBonds to link NC to OH once bonds formed.
     #mods COlist  (stored [C,O] by index)
-    if (wordList[0] in Clist):
+    if (int(wordList[0]) in Clist):
         bondnum = wordList[3]       #gets number of bond this atom has.
         for i in range(bondnum):
             if (wordList[3 + i] in Olist):
@@ -74,22 +79,23 @@ def getCO (Clist,Olist,wordList):
 
 def getNH(Nlist,Hlist,wordList):
     #this should pull NH pairs from timestep 0
-    if (wordList[0] in Nlist):
+    if (int(wordList[0]) in Nlist):
         bondnum = wordList[3]       #gets number of bond this atom has.
         for i in range(bondnum):
             if (wordList[3 + i] in Hlist):
                 NHlist.append([wordList[0],wordList[3 + i]])
                 
-def findPairs(lineFile,Clist,Olist,Nlist,Hlist,wordList):
+def findPairs(lineFile,Clist,Olist,Nlist,Hlist):
     #calls getCO and getNH on every line in the first timestep
     for line in lineFile:       #loops through each line of the file
-        stored = False
         wordList = line.split()
         if (wordList[0] == '#' and wordList[1] == 'Timestep'):      # the hashtag starts the header area
-            currStep = wordList[2] 
-        elif (currStep == 0):
+            currStep = int(wordList[2]) 
+        if (currStep == 0):
             getCO(Clist,Olsit,wordList)
             getNH(Nlist,Hlist,wordList)
+        elif(currStep > 0):
+            break
             
             
 def findCH(Clist, CHlist):
