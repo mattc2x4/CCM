7f6c1dff78d24782a1784b24b557fc378bb9d100
mattc2x4@yahoo.com <mattc2x4@yahoo.com>
2019-11-14 15:14:26 -0500

    BondAngleAnalysis

--- BondAngleAnalysis/findBondAngles.py
+++ BondAngleAnalysis/findBondAngles.py
@@ -27,17 +27,18 @@ end2V = []
 
 angList = []  #this will hold all angle vals calculated below. 
 
-boxdim = []     #[x,y,z] lengths
+boxdim = [0,0,0]     #[x,y,z] lengths
 
 def main():
+    print(boxdim[0])
     cosLaw([[0,0,0],[7,1,0]],[[0,0,0],[5,5,0]])
+    
 
 
 def distance(end1V, end2V):
     # this function is used to calculate the distance between 2 atoms.
     #boxdim has x, y, z dimensions of box as array, 0 = x, 1 = y, 2 = z
     #see reac.f "dista2" function
-    boxdim = [100,100,100]
     dx = end1V[0] - end2V[0]
     dy = end1V[1] - end2V[1]
     dz = end1V[2] - end2V[2]
